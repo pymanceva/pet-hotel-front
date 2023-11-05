0add9af97c75eaa241c944c778499fd1605b3cbb
Sebastian Holmes <87372096+Sebholmes@users.noreply.github.com>
2023-11-05 19:18:20 +0300

    Grid of rooms added (#2)

---
+++ src/entities/room/ui/RoomItem.tsx
@@ -0,0 +1,43 @@
+import { memo } from 'react';
+import { Box, Grid, Text, Title } from '@mantine/core';
+import { IRequestForRoomCreation } from '@/shared/types/request';
+
+const RoomItem: React.FC<IRequestForRoomCreation> = ({
+  number,
+  price,
+  size,
+  type,
+  isAvailable,
+}) => {
+  return (
+    <Box
+      style={{
+        display: 'flex',
+        alignItems: 'center',
+        flexDirection: 'column',
+        padding: '20px',
+        border: '1px solid #ccc',
+        borderRadius: '8px',
+        boxShadow: '0px 2px 4px rgba(0, 0, 0, 0.1)',
+      }}
+    >
+      <Title order={5}>Room №{number}</Title>
+      <Grid gutter={{ base: 5, xs: 'md', md: 'xl', xl: 50 }}>
+        <Grid.Col span={6}>
+          <Text size="md">Price: {price}</Text>
+        </Grid.Col>
+        <Grid.Col span={6}>
+          <Text size="md">Size: {size}m^2</Text>
+        </Grid.Col>
+        <Grid.Col span={6}>
+          <Text size="md">Type: {type}</Text>
+        </Grid.Col>
+        <Grid.Col span={6}>
+          <Text size="md">{isAvailable ? 'Available' : 'Unavailable'}</Text>
+        </Grid.Col>
+      </Grid>
+    </Box>
+  );
+};
+
+export default memo(RoomItem);
