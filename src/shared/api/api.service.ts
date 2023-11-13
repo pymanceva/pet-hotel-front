4ea85d1442f3f41b484a31ee424de05de9368e4a
azawert <53710085+azawert@users.noreply.github.com>
2023-11-13 10:46:26 +0300

    feat: добавил удаление комнаты

--- src/shared/api/api.service.ts
+++ src/shared/api/api.service.ts
@@ -1,7 +1,17 @@
 import ky from 'ky';
+import Cookies from 'js-cookie';
 import { API_URL } from '../const/const';
 
 export const api = ky.create({
   prefixUrl: API_URL,
-  headers: { 'X-PetHotel-User-Id': '1' },
+  hooks: {
+    beforeRequest: [
+      (request) => {
+        const userId = Cookies.get('X-PetHotel-User-Id');
+        if (userId) {
+          request.headers.set('X-PetHotel-User-Id', userId);
+        }
+      },
+    ],
+  },
 });
